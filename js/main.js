7c217d13801712565b860877d112c53f9a73b1a3
bigeppo <carlog91cast@gmail.com>
2022-06-28 14:08:45 +0200

    MILESTONE 1: intro

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// // Consegna
+// Viene fornita una piccola tabella che rappresenta i membri di un team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
